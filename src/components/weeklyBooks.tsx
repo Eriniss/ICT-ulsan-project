0abf1a3b373e286c8e81d47947e118413cc2108f
Eriniss <jhs2195@gmail.com>
2022-11-08 01:50:42 +0900

    메인페이지 이주의도서 컴포넌트 리팩토링

--- src/components/weeklyBooks.tsx
+++ src/components/weeklyBooks.tsx
@@ -1,105 +1,86 @@
-import { Container } from "react-bootstrap";
 import styled from "styled-components";
-import BannerCarousel from "./carousel";
 import BookImg1 from "../images/book1.png";
 import BookImg2 from "../images/book2.png";
 import BookImg3 from "../images/book3.png";
 import BookImg4 from "../images/book4.png";
 import BookImg5 from "../images/book5.png";
 
-const Banner = styled.div`
-  width: 90%;
-  height: 400px;
+const Container = styled.div`
+  width: 80%;
+  height: 100%;
   background-color: #f5f5f5;
+  margin-top: 50px;
+  border-radius: 15px;
   padding: 10px;
-  margin-top: 30px;
-  margin-bottom: 30px;
-  margin-left: 5%;
-  margin-right: 5%;
-  border-radius: 20px;
+  align-items: left;
+  display: block;
 `;
 
-const Title = styled.h2`
+const BannerTitle = styled.h2`
   font-size: 2rem;
   font-weight: 700;
-  margin-top: 10px;
+  margin: 50px 0 50px 0;
   text-align: center;
   width: 100%;
 `;
 
 const BannerItem = styled.div`
-  width: 20%;
-  height: 30%;
-  background-color: #f5f5f5;
-  padding: 0;
-  margin: 60px 0px 30px 0px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 20px;
-  float: left;
+  width: 25%;
+  padding: 10px;
+  margin: 0px;
+  display: inline-block;
+  vertical-align: top;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    width: 50%;
+  }
+
+  @media (max-width: 480px) {
+    width: 100%;
+  }
+
+  @media (min-width: 1024px) {
+    width: 20%;
+  }
 `;
 
-const  BannerBox = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #f5f5f5;
-  padding: 0;
-  margin: 0;
-`;
+const BannerBody = styled.p`
+  font-size: 1.5rem;
+  font-weight: 700;
+`
 
 const BookImg = styled.img`
-  width: 40%;
-  height: 100%;
-  margin-bottom: 20px;
-
-  @media (max-width: 1440px) {
-    width: 70%;
-    height: 100%;
-    margin-bottom: 20px;
-  }
+  width: 100px;
+  height: 120px;
+  margin: 0 20px 20px 20px;
 `;
 
 function WeeklyBooks() {
   return (
-      <Banner>
-        <Title>이주의 도서</Title>
-        <BannerItem>
-          <BannerBox>
-            <BookImg src={BookImg1}/>
-            <h5>나는 나로 살기로 했다</h5>
-            <p>★★★☆☆</p>
-          </ BannerBox>
-        </BannerItem>
-        <BannerItem>
-          <BannerBox>
-            <BookImg src={BookImg2}/>
-            <h5>지적 대화를 위한 넓고 얕은 지식</h5>
-            <p>★★★★★</p>
-          </ BannerBox>
-        </BannerItem>
-        <BannerItem>
-          <BannerBox>
-            <BookImg src={BookImg3}/>
-            <h5>달러구트 꿈백화점</h5>
-            <p>★★★★☆</p>
-          </ BannerBox>
-        </BannerItem>
-        <BannerItem>
-          <BannerBox>
-            <BookImg src={BookImg4}/>
-            <h5>불편한 편의점</h5>
-            <p>★★★☆☆</p>
-          </ BannerBox>
-        </BannerItem>
-        <BannerItem>
-          <BannerBox>
-            <BookImg src={BookImg5}/>
-            <h5>언어의 온도</h5>
-            <p>★★★★☆</p>
-          </ BannerBox>
-        </BannerItem>
-      </Banner>
+    <Container>
+      <BannerTitle>이주의 도서</BannerTitle>
+      <BannerItem>
+        <BookImg src={BookImg1}/>
+        <BannerBody>나는 나로 살기로 했다</BannerBody>
+      </BannerItem>
+      <BannerItem>
+        <BookImg src={BookImg2}/>
+        <BannerBody>지적 대화를 위한 넓고 얕은 지식</BannerBody>
+      </BannerItem>
+      <BannerItem>
+        <BookImg src={BookImg3}/>
+        <BannerBody>달러구트 꿈백화점</BannerBody>
+      </BannerItem>
+      <BannerItem>
+          <BookImg src={BookImg4}/>
+          <BannerBody>불편한 편의점</BannerBody>
+      </BannerItem>
+      <BannerItem>
+        <BookImg src={BookImg5}/>
+        <BannerBody>언어의 온도</BannerBody>
+      </BannerItem>
+    </Container>
   );
 }
 
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,12 +2,6 @@ import axios from "axios";
 import Carousel from '../components/carousel';
 import WeeklyBooks from "../components/weeklyBooks";
 
-interface dataType {
-  _id: any;
-  title: string;
-  body: string;
-}
-
 const Main = () => {
   axios
   .get("http://localhost:4000")
@@ -20,7 +14,7 @@ const Main = () => {
   );
   
   return (
-    <div className="Main">
+    <div className="Main" style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
       <Carousel />
       <WeeklyBooks />
     </div>
